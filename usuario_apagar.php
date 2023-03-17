d6054fd2a01151f8f0998ad1e8cc2a66b16aa091
Rafael Jaques <rafael@phpit.com.br>
2023-03-17 11:07:52 -0300

    Remoção de usuários

--- usuario_apagar.php
+++ usuario_apagar.php
@@ -1,6 +1,28 @@
 <?php
     # usuario_apagar.php
-
     require('twig_carregar.php');
+    require('pdo.inc.php'); // Conexão com o banco
 
-    echo $twig->render('usuario_apagar.html',);
+    // Rotina de POST - Apagar o usuário
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        // Modifica o usuário para ativo = 0
+        $id = $_POST['id'] ?? false;
+        if ($id) {
+            $sql = $pdo->prepare('UPDATE usuarios SET ativo = 0 WHERE id = ?');
+            $sql->execute([$id]);
+        }
+        header('location:usuarios.php');
+        die;
+    }
+
+    // Rotina de GET - Mostrar informações na tela
+
+    // Busca o usuário no banco para mostrar o nome dele na tela
+    $id = $_GET['id'] ?? false;
+    $sql = $pdo->prepare('SELECT * FROM usuarios WHERE id = ?');
+    $sql->execute([$id]);
+    $usuario = $sql->fetch(PDO::FETCH_ASSOC);
+
+    echo $twig->render('usuario_apagar.html',[
+        'usuario' => $usuario,
+    ]);
