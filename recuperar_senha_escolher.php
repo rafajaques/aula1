4e3dcea052d953fa894664c63626b446b762a1f5
Rafael Jaques <rafael@phpit.com.br>
2023-03-24 09:51:39 -0300

    Atualização de trocar senha

--- recuperar_senha_escolher.php
+++ recuperar_senha_escolher.php
@@ -1,39 +1,44 @@
 <?php
-    # recuperar_senha_escolher.php
-    require('twig_carregar.php');
-    require('pdo.inc.php');
+# recuperar_senha_escolher.php
+require('twig_carregar.php');
+require('pdo.inc.php');
 
-    $token = $_GET['token'] ?? $_POST['token'] ?? false;
+$token = $_GET['token'] ?? $_POST['token'] ?? false;
 
-    if (!$token) {
-        header('location:login.php');
-        die;
-    }
+if (!$token) {
+    header('location:login.php');
+    die;
+}
 
-    // Rotina de POST - Recuperar senha
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $password = $_POST['senha'] ?? false;
-        $password = trim($password);
-        // Tratamento para o caso de senha falsa
-        // ....
-        
+// Rotina de POST - Recuperar senha
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $password = $_POST['senha'] ?? false;
+    $password_confi = $_POST['senha_confirma'] ?? false;
+
+    $password = trim($password);
+    $password_confi = trim($password_confi);
+    // Tratamento para o caso de senha falsa
+    // ....
+    if ($password == $password_confi) {
         $sql = $pdo->prepare('UPDATE usuarios SET senha = :senha, recupera_token = NULL WHERE recupera_token = :token');
         $sql->execute([
             ':senha' => password_hash($password, PASSWORD_BCRYPT),
             ':token' => $token,
         ]);
-
         header('location:login.php?erro=3');
-
         die;
     }
 
-    $sql = $pdo->prepare('SELECT * FROM usuarios WHERE recupera_token = ?');
-    $sql->execute([$token]);
+    $msg = 'Conseguiu errar a senha, né?';
 
-    if ($sql->rowCount() == 1) {
-        echo $twig->render('recuperar_senha_escolher.html', ['token' => $token]);
-    } else {
-        header('location:login.php');
-        die;
-    }
+}
+
+$sql = $pdo->prepare('SELECT * FROM usuarios WHERE recupera_token = ?');
+$sql->execute([$token]);
+
+if ($sql->rowCount() == 1) {
+    echo $twig->render('recuperar_senha_escolher.html', ['token' => $token, 'msg' => $msg ?? false]);
+} else {
+    header('location:login.php');
+    die;
+}
