a0a173ce4811df9f7142b928f4ae5b3ea0e83b73
Rafael Jaques <rafael@phpit.com.br>
2023-04-14 11:04:25 -0300

    Roteador

---
+++ router.php
@@ -0,0 +1,12 @@
+<?php
+# router.php
+$pagina = $_GET['pagina'] ?? false;
+
+$include = filter_var("{$pagina}.php", FILTER_SANITIZE_STRING);
+
+if (!file_exists($include)) {
+    echo 'Seu raquer!';
+    die;
+}
+
+require($include);
