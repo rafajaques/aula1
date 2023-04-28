d105fd1b81c7c2980885b120cab464b7ae6c5afd
Rafael Jaques <rafael@phpit.com.br>
2023-04-28 11:02:35 -0300

    Atualização do template

--- verifica_login.php
+++ verifica_login.php
@@ -2,6 +2,6 @@
     # verifica_login.php
     session_start();
     if (!isset($_SESSION['user'])) {
-        header('location:login.php?erro=2');
+        header('location:/login');
         die;
     }
